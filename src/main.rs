b736c343b7ced77f8cfb93c1a66c5b23867c8cb2
Jay Schuster <spok.real@gmail.com>
2023-01-02 23:36:25 -0600

    add some testing in main

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,7 @@ use std::io;
 
 fn main() {
     let mut game: Game = Game::new_game();
+    // let mut game2 = Game::default();
     println!("{}", game.to_string());
+    // println!("{}", game2.to_string());
 }
