61e7e6fffca4d5317c4058ac9a7fe6f27d57a5af
Jay Schuster <spok.real@gmail.com>
2023-01-03 21:16:15 -0600

    add quitting

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,15 @@ fn main() {
     // main game loop
     while !game.is_over() {
         println!("{}", game.to_string());
-        println!("Enter move (w/a/s/d):");
+        println!("Enter move (w/a/s/d) or q to quit:");
         let mut move_ = String::new();
         io::stdin().read_line(&mut move_).expect("Failed to read line");
         let move_= move_.trim().to_lowercase();
         let move_ = move_.as_str();
+
+        if move_ == "q" {
+            break;
+        }
 
         match Game::parse_move(move_) {
             Ok(dir) => game.game_move(dir),
